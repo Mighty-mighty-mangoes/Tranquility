65afa46ec497359b58a931002eb76bf65606b238
Gracie Ingermanson <gmi@umich.edu>
2021-01-13 22:41:12 -0500

    Moving cart route to a new branch

--- server/api/cart.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {User, CartItem} = require('../db/models');
-module.exports = router;
-
-router.get('/', async (req, res, next) => {
-  console.log(req.session.user);
-  res.sendStatus(200);
-});
