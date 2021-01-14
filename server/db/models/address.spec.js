e716ebd10b87fc2f05af628090080c6ab90a7959
heathernoto <heather.noto@gmail.com>
2021-01-14 10:52:29 -0500

    Fixed Tests, single and all candle express routes confirmed working

--- server/db/models/address.spec.js
+++ server/db/models/address.spec.js
@@ -1,34 +1,28 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const db = require('../index')
-const Address = db.model('Address')
+const {expect} = require('chai');
+const db = require('../index');
+const Address = db.model('address');
 
 describe('Address model', () => {
   beforeEach(() => {
-    return db.sync({force: true})
-  })
+    return db.sync({force: true});
+  });
 
-  describe('instanceMethods', () => {
-    beforeEach(async () => {
-      it('returns an error if the wrong zip code is given', async () => {
-        try {
-          let wrongAddress = await Address.create({
-            houseNumber: 123,
-            streetName: 'Cherry Tree Lane',
-            city: 'Beverly Hills',
-            state: 'California',
-            zipCode: 45
-          })
-          if (wrongAddress) {
-            throw Error('Validation should fail on a non-zip-code')
-          }
-        } catch (err) {
-          expect(err.message).to.not.have.string(
-            'Validation should have failed'
-          )
-        }
-      })
-    }) // end describe('correctPassword')
-  }) // end describe('instanceMethods')
-}) // end describe('User model')
+  it('returns an error if the wrong zip code is given', async () => {
+    try {
+      let wrongAddress = await Address.create({
+        houseNumber: 123,
+        streetName: 'Cherry Tree Lane',
+        city: 'Beverly Hills',
+        state: 'California',
+        zipCode: 45,
+      });
+      if (wrongAddress) {
+        throw Error('Validation should fail on a non-zip-code');
+      }
+    } catch (err) {
+      expect(err.message).to.not.have.string('Validation should have failed');
+    }
+  });
+});
--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -1,91 +1,39 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const db = require('../index')
-const User = db.model('user')
+const {expect} = require('chai');
+const db = require('../index');
+const User = db.model('user');
 
 describe('User model', () => {
   beforeEach(() => {
-    return db.sync({force: true})
-  })
+    return db.sync({force: true});
+  });
 
-  describe('instanceMethods', () => {
-    describe('correctPassword', () => {
-      let cody
-
-      beforeEach(async () => {
-        cody = await User.create({
-          email: 'cody@puppybook.com',
-          password: 'bones',
-        })
-      })
-
-      it('returns true if the password is correct', () => {
-        expect(cody.correctPassword('bones')).to.be.equal(true)
-      })
-
-      it('returns false if the password is incorrect', () => {
-        expect(cody.correctPassword('bonez')).to.be.equal(false)
-      })
-    }) // end describe('correctPassword')
-  }) // end describe('instanceMethods')
-
-  //USER FULLNAME
-  describe('user', () => {
-    describe('fullName', () => {
-      let cody
-
-      beforeEach(async () => {
-        cody = await User.create({
-          firstName: 'jennifer',
-          lastName: 'franco',
-        })
-      })
-      it('fullName should be formatted corrently', () => {
-        expect(cody.fullName('jennifer', 'franco')).to.deep.equal(
-          'Jennifer Franco'
-        )
-      })
-    })
-  }) // end describe('fullName')
-
-  //EMAIL FIELD---HEATHER might need to fix this to define
-  describe('email', () => {
-    it('email cannot be empty', async () => {
-      // We shouldn't be able to create a user without an email address.
-      await expect(
-        User.create({email: ''}),
-        "We shouldn't be able to create a user with no email"
-      ).to.be.rejected
-    })
-
-    it('email must be unique', async () => {
-      // We shouldn't be able to create two users with the same name.
-      await User.create({email: 'cody@gmail.com'})
-      await expect(
-        User.create({email: 'cody@gmail.com'}),
-        "Shouldn't be able to create two users with the same name "
-      ).to.be.rejected
-    })
-  })
-
-  //NAME FIELD-HEATHER=> have to see if this is written correctly
-
-  describe('firstName', () => {
-    it('firstName cannot be empty', async () => {
-      // We shouldn't be able to create a user without a firstName.
-      await expect(
-        User.create({firstName: ''}),
-        "We shouldn't be able to create a user with no firstName"
-      ).to.be.rejected
-    })
-
-    it('lastName cannot be empty', async () => {
-      // We shouldn't be able to create two users with the same name.
-      await expect(
-        User.create({lastName: ''}),
-        "Shouldn't be able to create a user with no lastName "
-      ).to.be.rejected
-    })
-  })
-}) // end describe('User model')
+  const data = {
+    firstName: 'cody',
+    lastName: 'pug',
+    email: 'cody@puppybook.com',
+    password: 'bones',
+  };
+  it('returns true if the password is correct', async () => {
+    const cody = await User.create(data);
+    expect(cody.correctPassword('bones')).to.be.equal(true);
+  });
+    it('returns false if the password is incorrect', async () => {
+      const cody = await User.create(data);
+      expect(cody.correctPassword('bonez')).to.be.equal(false);
+    });
+    it('users default to false for admin', async () => {
+      const cody = await User.create(data);
+      expect(cody.isAdmin).to.be.equal(false);
+    });
+  it('first and last name are capitalized', async () => {
+    const cody = await User.create(data);
+    expect(cody.firstName).to.be.equal('Cody');
+    expect(cody.lastName).to.be.equal('Pug');
+  });
+  it('virtual fullname to be correct', async () => {
+    const cody = await User.create(data);
+    expect(cody.fullName).to.be.equal('Cody Pug');
+  });
+});
