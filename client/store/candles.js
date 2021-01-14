d0d69152e47ebe117fc75ece0813f5f296418d04
heathernoto <heather.noto@gmail.com>
2021-01-14 17:25:59 -0500

    feat(allCandles component) add link to route/ candles.map in progress

--- client/store/candles.js
+++ client/store/candles.js
@@ -1,55 +1,56 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const SET_CANDLES = 'SET_CANDLES'
-const SINGLE_CANDLE = 'SINGLE_CANDLE'
+const SET_CANDLES = 'SET_CANDLES';
+const SINGLE_CANDLE = 'SINGLE_CANDLE';
 
 export const setCandles = (candles) => ({
   type: SET_CANDLES,
   candles,
-})
+});
 
 export const singleCandle = (candle) => ({
   type: SINGLE_CANDLE,
   candle,
-})
+});
 
 //get all candles
 export const fetchCandles = () => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get('/api/candles')
-      dispatch(setCandles(data))
+      const {data} = await axios.get('/api/candles');
+      dispatch(setCandles(data));
     } catch (err) {
-      console.log('Something is wrong in the all candles thunk: ', err)
+      console.log('Something is wrong in the all candles thunk: ', err);
     }
-  }
-}
+  };
+};
 //get single candle
-export const fetchCandle = (id) => {
+export const fetchSingleCandle = (id) => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`/api/candles/${id}`)
-      dispatch(singleCandle(data))
+      const {data} = await axios.get(`/api/candles/${id}`);
+      console.log('fetch single candle thunk', data);
+      dispatch(singleCandle(data));
     } catch (err) {
-      console.log('Something is wrong in the single candles thunk: ', err)
+      console.log('Something is wrong in the single candles thunk: ', err);
     }
-  }
-}
+  };
+};
 let initialState = {
   singleCandle: {},
   candles: [],
-}
+};
 export default function candleReducer(state = initialState, action) {
-  //console.log('candle reducer', action.type);
+  console.log('fetch candleReducer', action.type, 'state', state);
   switch (action.type) {
     case SET_CANDLES:
-      return action.candles
+      return action.candles;
 
     case SINGLE_CANDLE:
-      return {...state, singleCandle: action.candle}
+      return {...state, singleCandle: action.candle};
 
     default:
-      return state
+      return state;
   }
 }
 //thought this was needed?  return null
