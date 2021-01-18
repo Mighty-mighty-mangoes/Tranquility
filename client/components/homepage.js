52c8c775bce29683d5723b7695174ee1406c8661
Katie Weinstein <katie.weinstein3@gmail.com>
2021-01-18 17:02:40 -0500

    feat(login and signup): certain components persist user data through refreshes, fully functional, small design changes

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {me} from '../store/user';
 
-const Homepage = ({isLoggedIn, name}) => (
-  <div>
-    <h1>tranquility: candles for finding your peace</h1>
-    {isLoggedIn ? (
-      <p className="text-center">Welcome back, {name}!</p>
-    ) : (
-      <p className="text-center">Welcome to our little shop!</p>
-    )}
-    <div className="d-flex justify-content-center">
-      <img
-        src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/01/17/11/istock-994807816.jpg"
-        className="img-fluid featureImage rounded"
-        alt="candles"
-      />
-    </div>
-  </div>
-);
+class Homepage extends React.Component {
+  async componentDidMount() {
+    await this.props.loadUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>tranquility: candles for finding your peace</h1>
+        {this.props.isLoggedIn ? (
+          <p className="text-center">Welcome back, {this.props.name}!</p>
+        ) : (
+          <p className="text-center">Welcome to our little shop!</p>
+        )}
+        <div className="d-flex justify-content-center">
+          <img
+            src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/01/17/11/istock-994807816.jpg"
+            className="img-fluid featureImage rounded"
+            alt="candles"
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapState = (state) => {
   return {
@@ -26,4 +35,8 @@ const mapState = (state) => {
   };
 };
 
-export default connect(mapState)(Homepage);
+const mapDispatch = (dispatch) => ({
+  loadUser: () => dispatch(me()),
+});
+
+export default connect(mapState, mapDispatch)(Homepage);
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,82 +1,90 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {me} from '../store/user';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
 import logo from '../../public/logo.png';
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <img src={logo} className="img-fluid d-block" />
-    <nav
-      className="navbar navbar-expand-sm navbar-light"
-      style={{backgroundColor: '#BEDCFE'}}
-    >
-      <ul className="navbar-nav nav-justified">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            id="navbarDropdownMenuLink"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Shop
-          </a>
-          <ul
-            className="dropdown-menu"
-            aria-labelledby="navbarDropdownMenuLink"
-          >
-            <li>
-              <Link to="/listAllCandles" className="dropdown-item">
-                All Candles
+class Navbar extends React.Component {
+  async componentDidMount() {
+    await this.props.loadUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={logo} className="img-fluid d-block" />
+        <nav
+          className="navbar navbar-expand-sm navbar-light"
+          style={{backgroundColor: '#BEDCFE'}}
+        >
+          <ul className="navbar-nav nav-justified">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
               </Link>
             </li>
-            <li>
-              <a className="nav-link dropdown-item">Food</a>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Shop
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <Link to="/listAllCandles" className="dropdown-item">
+                    All Candles
+                  </Link>
+                </li>
+                <li>
+                  <a className="nav-link dropdown-item">Food</a>
+                </li>
+                <li>
+                  <a className="nav-link dropdown-item">Flowers</a>
+                </li>
+                <li>
+                  <a className="nav-link dropdown-item">Spices</a>
+                </li>
+                <li>
+                  <a className="nav-link dropdown-item">Candles for Coders</a>
+                </li>
+              </ul>
             </li>
-            <li>
-              <a className="nav-link dropdown-item">Flowers</a>
+            <li className="nav-item">
+              <a className="nav-link">About Us</a>
             </li>
-            <li>
-              <a className="nav-link dropdown-item">Spices</a>
+            <li className="nav-item">
+              <a className="nav-link">Contact</a>
             </li>
-            <li>
-              <a className="nav-link dropdown-item">Candles for Coders</a>
-            </li>
+            {this.props.isLoggedIn ? (
+              <li className="nav-item">
+                <a className="nav-link" onClick={this.props.handleClick}>
+                  Logout
+                </a>
+              </li>
+            ) : (
+              <li className="row nav-item">
+                <Link to="/login" className="col nav-link">
+                  Log In
+                </Link>
+                <Link to="/signup" className="col nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            )}
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">About Us</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">Contact</a>
-        </li>
-        {isLoggedIn ? (
-          <li className="nav-item">
-            <a className="nav-link" onClick={handleClick}>
-              Logout
-            </a>
-          </li>
-        ) : (
-          <li className="row nav-item">
-            <Link to="/login" className="col nav-link">
-              Log In
-            </Link>
-            <Link to="/signup" className="col nav-link">
-              Sign Up
-            </Link>
-          </li>
-        )}
-      </ul>
-    </nav>
-  </div>
-);
+        </nav>
+      </div>
+    );
+  }
+}
 
 /**
  * CONTAINER
@@ -92,15 +100,8 @@ const mapDispatch = (dispatch) => {
     handleClick() {
       dispatch(logout());
     },
+    loadUser: () => dispatch(me()),
   };
 };
 
 export default connect(mapState, mapDispatch)(Navbar);
-
-/**
- * PROP TYPES
- */
-// Navbar.propTypes = {
-//   handleClick: PropTypes.func.isRequired,
-//   isLoggedIn: PropTypes.bool.isRequired,
-// };
