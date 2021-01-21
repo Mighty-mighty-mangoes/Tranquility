2f04ee072b01f92114164ded1ccda2e79391bd57
Katie Weinstein <katie.weinstein3@gmail.com>
2021-01-21 11:21:06 -0500

    last minute fixes

--- client/store/index.js
+++ client/store/index.js
@@ -1,5 +1,4 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {createLogger} from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import user from './user';
@@ -17,9 +16,7 @@ const reducer = combineReducers({
   coderCandles: candleReducer,
 });
 
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-);
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
 const store = createStore(reducer, middleware);
 
 export default store;
